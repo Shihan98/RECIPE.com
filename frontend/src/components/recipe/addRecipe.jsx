62b84a49eaa2dc5b5126836a50d3b0b0bc52dba1
Shihan98 <shihanthathsara98@gmail.com>
2023-11-07 23:32:09 +0530

    feat: create add modal

---
+++ frontend/src/components/recipe/addRecipe.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import AddModal from "../modal/addModal";
+
+export default function AddRecipe() {
+  return (
+    <div className="bg-cyan-100">
+    <div className="grid grid-cols-6 gap-4 container mx-auto">
+      <div className="col-start-1 col-end-3 ...">
+        <AddModal/>
+        </div>
+      <div className="col-end-7 col-span-4 ...">
+        <p>g ksjsbci k scisbcjksbc s cj skc s c s cj s c</p>
+      </div>
+    </div>
+    </div>
+  );
+}
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../components/heroSection/hero";
-import RecipeSection from "../components/recipe/RecipeSection";
+import RecipeSection from "../components/recipe/recipeSection";
+import AddRecipe from "../components/recipe/addRecipe";
 
 export default function Homepage() {
   return (
     <>
       <Hero />
       <RecipeSection />
+      <AddRecipe/>
     </>
   );
 }
