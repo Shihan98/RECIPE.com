54374d0918202aea60302e025eb5eb8ef2af002c
Shihan98 <shihanthathsara98@gmail.com>
2023-11-08 09:58:16 +0530

    refactor: finish add recipe section styling

--- frontend/src/components/modal/addModal.jsx
+++ frontend/src/components/modal/addModal.jsx
@@ -18,9 +18,9 @@ export default function AddModal() {
         <button
           type="button"
           onClick={openModal}
-          className="rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
+          className="rounded-md bg-cyan-600 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
         >
-          Open dialog
+          Add Recipe
         </button>
       </div>
 
--- frontend/src/components/recipe/addRecipe.jsx
+++ frontend/src/components/recipe/addRecipe.jsx
@@ -3,15 +3,19 @@ import AddModal from "../modal/addModal";
 
 export default function AddRecipe() {
   return (
-    <div className="bg-cyan-100">
-    <div className="grid grid-cols-6 gap-4 container mx-auto">
-      <div className="col-start-1 col-end-3 ...">
-        <AddModal/>
+    <div className="bg-cyan-100 flex">
+      <div className="grid grid-cols-6 gap-4 container mx-auto p-5 py-8">
+        <div className="self-center col-start-1 col-end-2 ... ">
+          <AddModal />
         </div>
-      <div className="col-end-7 col-span-4 ...">
-        <p>g ksjsbci k scisbcjksbc s cj skc s c s cj s c</p>
+        <div className="col-end-7 col-span-5 ...">
+          <p className="font-medium">Ready to add your own twist?</p>
+          <p className="font-thin">
+            Click 'Add a Recipe' to share your culinary creations with the
+            community!
+          </p>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
