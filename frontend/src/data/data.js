1a995532dec9b48a1a292a9d7469cebfa5f9af74
Shihan98 <shihanthathsara98@gmail.com>
2023-11-09 01:14:16 +0530

    feat: add dummy data to data.js file

--- frontend/src/data/data.js
+++ frontend/src/data/data.js
@@ -1,23 +1,23 @@
 export const recipeData = [
   {
     id: 1,
-    recipeName: "Recipe 1",
+    recipeName: "Savory Spinach and Feta Stuffed Chicken",
     ingredients: "Chicken breasts, Spinach, Feta cheese, Olive oil",
     description:
       "A delicious and healthy dish that's quick to prepare. Perfect for a cozy dinner or entertaining guests.",
   },
   {
     id: 2,
-    recipeName: "Recipe 2",
-    ingredients: "Chicken breasts, Spinach, Feta cheese, Olive oil",
+    recipeName: "Grilled Lemon Herb Salmon",
+    ingredients: "Salmon fillets, Lemon, Fresh herbs, Olive oil, Salt, Pepper",
     description:
-      "A delicious and healthy dish that's quick to prepare. Perfect for a cozy dinner or entertaining guests.",
+      "A light and flavorful salmon dish that's perfect for a summer barbecue. The combination of zesty lemon and fresh herbs will delight your taste buds.",
   },
   {
     id: 3,
-    recipeName: "Recipe 3",
-    ingredients: "Chicken breasts, Spinach, Feta cheese, Olive oil",
+    recipeName: "Spaghetti Carbonara Mushroom Risotto",
+    ingredients: "Spaghetti, Pancetta, Eggs, Parmesan cheese, Black pepper",
     description:
-      "A delicious and healthy dish that's quick to prepare. Perfect for a cozy dinner or entertaining guests.",
+      "A classic Italian pasta dish that's simple yet incredibly delicious. Creamy carbonara sauce, crispy pancetta, and al dente spaghetti make this a go-to recipe.",
   },
 ];
